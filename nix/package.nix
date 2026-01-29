b159ff40fa7a0a4ef43370888aa2bb387cbf48fb
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-01-29 22:24:14 +0800

    v2.20.78

--- nix/package.nix
+++ nix/package.nix
@@ -38,7 +38,7 @@
   soundfont-path ? "${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2",
 }:
 let
-  version = "2.20.77";
+  version = "2.20.78";
   pname = "space-station-14-launcher";
 in
 buildDotnetModule rec {
